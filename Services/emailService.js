31493e15d646db00beed0755d9194e6e9ce19269
muhammadfaizan027915 <muhammadfaizan027915@gmail.com>
2023-02-02 23:08:56 +0500

    email sender feature created

---
+++ Services/emailService.js
@@ -0,0 +1,41 @@
+const emailer = require("nodemailer");
+const hbs = require("nodemailer-express-handlebars");
+
+module.exports = (from, to, subject, name, role, message) => {
+  const transport = emailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: "SSL",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASS,
+    },
+  });
+
+  transport.use(
+    "compile",
+    hbs({
+      viewEngine: {
+        extname: ".handlebars",
+        partialsDir: "./Views",
+        defaultLayout: false,
+      },
+      viewPath: "./Views",
+      extname: ".handlebars",
+    })
+  );
+
+  return transport.sendMail({
+    subject,
+    envelope: {
+      from: `${from} <${from}>`,
+      to: `${to} <${to}>`
+    },
+    template: "index",
+    context: {
+      name,
+      role,
+      message,
+    },
+  });
+};
--- index.js
+++ index.js
@@ -1,9 +1,43 @@
-const express = require("express")
-const app = express()
+const emailer = require("./Services/emailService");
+const handlebars = require("express-handlebars");
+const dotenv = require("dotenv");
+const express = require("express");
+const app = express();
+const path = require("path");
 
-const PORT = process.env.PORT || 4000
+// Config Dotenv
+dotenv.config();
+app.use(express.json());
 
+// Setup View Engine
+app.engine(".hbs", handlebars.engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", "./Views");
+
+// Email route
+app.post("/send-email", (req, res) => {
+  const { from, to, subject, name, role, message } = req.body;
+
+  if (!from || !to || !subject || !message)
+    return res
+      .status(400)
+      .json({ message: "Please provide all the required fields!" });
+
+  emailer(from, to, subject, name, role, message)
+    .then((info) => {
+      res.status(200).json({
+        message: "Eamil sent successfully!",
+        messageId: info.messageId,
+      });
+    })
+    .catch((error) => {
+      res.status(400).send(error.message);
+    });
+});
+
+// PORT to run the server
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running at port ${PORT}`)
-})
+  console.log(`Server is running at port ${PORT}`);
+});
