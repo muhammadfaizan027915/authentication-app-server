775833b8c2cda3b7357edd71bf8866dce723a355
muhammadfaizan027915 <muhammadfaizan027915@gmail.com>
2023-02-02 20:08:03 +0500

    Creating the server

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const express = require("express")
 const app = express()
+
+const PORT = process.env.PORT || 4000
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`)
+})
